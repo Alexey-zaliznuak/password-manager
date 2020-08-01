132a8dd22fe59df9a261537394f86ffbae6e3bf9
Alexey-zaliznuak <zaliznuak20@gmail.com>
2020-08-01 12:46:24 +0400

    my commit

--- DataBase.py
+++ DataBase.py
@@ -21,3 +21,20 @@ class DataBase():
             if string["id"] == el_id:
                 del data[index]
         self.write(data)
+    def create(self, data):
+        old_data = self.get()
+        max_element = max(old_data, default={'id': 0}, key=lambda element: int(element['id']))
+        max_id = int(max_element["id"]) 
+        id_of_password  = max_id + 1
+
+        data_new = {
+        'service':f"{Service}",
+        'email':f"{Email}",
+        'password':f"{Password}",
+        'data_of_mod':f"{mod_time}",
+        'id':f"{id_of_password}"
+        }
+        
+        old_data += [data_new]
+        data = old_data
+        database.write(data)
